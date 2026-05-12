da40b9702e5994a8c2a8acb3c6bfe21f27df55fd
Hunter100102 <spc.cody.hunter@gmail.com>
2026-05-11 21:02:26 -0400

    Updates

--- app/src/main/java/com/glowcoach/MainActivity.kt
+++ app/src/main/java/com/glowcoach/MainActivity.kt
@@ -185,3 +185,35 @@ suspend fun sendChatMessage(message: String): String = withContext(Dispatchers.I
         "I couldn’t reach the backend. Make sure the server is running and the API URL is correct."
     }
 }
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GlowCoachTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GlowCoachTheme {
+        Greeting("Android")
+    }
+}
